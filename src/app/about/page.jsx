2c537d64560487ea5e5383216f55ccea1fad9798
marwa-nassane0052 <marwa-nassane0052>
2024-03-09 20:25:44 +0100

    commit front end project

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+
+import Header from "@/Components/Header/header"
+import { Button } from 'antd';
+import {ForwardOutlined } from '@ant-design/icons'
+
+export default function About() {
+    return (
+      <div>
+        this is about page
+        <Header/>
+        <Button></Button>
+        <ForwardOutlined />
+      </div>
+    )
+  }
+  
