9a1b7822f49264abcd9044e372ab23163aa29dca
wafaabek <162354801+wafaabek@users.noreply.github.com>
2024-03-21 17:44:44 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 
+import HomePage from "./homepage/page"
+import DetailsPage from "./detailspage/page"
 export default function Home() {
   return (
     <div>
-      this is home page
+       this is home page
+      {/*<HomePage></HomePage>*/}
+     
     </div>
   )
 }
+
